11463dafd37b804c4fa4e7b53bdf1016f232280d
calabr <stefanocalabretti@outlook.it>
2021-06-16 21:49:07 +0200

    Add circular_buffer

---
+++ lib/circular_buffer/circular_buffer.hpp
@@ -0,0 +1,115 @@
+#ifndef CIRCULAR_BUFFER_HPP_
+#define CIRCULAR_BUFFER_HPP_
+#pragma once
+
+#include <initializer_list>
+#include <array>
+#include <optional>
+
+namespace uni {
+  template <typename T, int MaxLength>
+  class CircularBuffer {
+  public:
+    CircularBuffer()
+      : front(0), count(0), buffer({})
+    {};
+
+    CircularBuffer(std::initializer_list<T> values): CircularBuffer() {
+      for (auto& v : values) {
+        tryPushFront(v);
+      }
+    }
+
+
+    bool tryPushFront(T value) noexcept {
+      if (isFull())
+        return false;
+
+      getFront() = value;
+      incrementFront();
+      count++;
+      return true;
+    }
+
+    bool tryPushBack(T value) noexcept {
+      if (isFull())
+        return false;
+
+      count++;
+      getBack() = value;
+      return true;
+    }
+
+
+    std::optional<T> tryPopFront() noexcept {
+      if (isEmpty())
+        return {};
+
+      count--;
+      decrementFront();
+      return getFront();
+    }
+
+    std::optional<T> tryPopBack() noexcept {
+      if (isEmpty())
+        return {};
+
+      const int last = 
+        getIndexOfLast();
+
+      count--;
+      return buffer[last];
+    }
+
+
+    [[nodiscard]]
+    int length() const {
+      return count;
+    }
+
+    [[nodiscard]]
+    bool isFull() const {
+      return count == MaxLength;
+    }
+
+    [[nodiscard]]
+    bool isEmpty() const {
+      return count == 0;
+    }
+
+  private:
+    void incrementFront() {
+      ++front %= MaxLength;
+    }
+
+    void decrementFront() {
+      front = --front < 0
+        ? MaxLength + front
+        : front;
+    }
+
+    T& getFront() {
+      return buffer[front];
+    }
+
+    T& getBack() {
+      return buffer[getIndexOfLast()];
+    }
+
+    [[nodiscard]]
+    int getIndexOfLast() const {
+      const int last = 
+        front - count;
+    
+      return last < 0
+        ? MaxLength + last
+        : last;
+    }
+
+    std::array<T, MaxLength> buffer;
+    int front;
+    int count;
+  };
+}
+
+#endif // !CIRCULAR_BUFFER_HPP_
